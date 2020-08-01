31c20c0cf8bdb9c966be7599e4544f9ec482588f
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-08-01 19:53:56 +0200

    .

--- resort/src/components/Room.js
+++ resort/src/components/Room.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import default_image from "../images/room-1.jpeg";
 
 export default function Room({ room }) {
   const { name, slug, images, price } = room;
 
-  return <article className="room">
+  return (
+    <article className="room">
       <div className="img-container">
-          <img src={images[0]} alt="single room" />
+        <img src={images[0] || default_image} alt="single room" />
+
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
+
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+            Features
+        </Link>
       </div>
-  </article>;
+    </article>
+  );
 }
