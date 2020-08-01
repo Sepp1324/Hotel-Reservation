78c1a69806834793c8767c9439590889a2e4f9a0
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-08-01 12:29:58 +0200

    Worked on Navbar

--- resort/src/App.js
+++ resort/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-import Home from "./pages/Home";
-import Rooms from "./pages/Rooms";
-import SingleRoom from "./pages/SingleRoom";
-import Error from "./pages/Error";
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
 
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom'
 
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
