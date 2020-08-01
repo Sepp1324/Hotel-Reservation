8d1ea1f6165d4cb8ab2711a2edc32dd6acda4616
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-08-01 16:54:58 +0200

    Added Services on Home

--- resort/src/pages/Home.js
+++ resort/src/pages/Home.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import Hero from '../components/Hero'
-import Banner from '../components/Banner'
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import Services from "../components/Services";
 
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return <Hero> 
-        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-        <Link to='/rooms' className="btn-primary">
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
             our rooms
-        </Link>
+          </Link>
         </Banner>
-    </Hero>;
+      </Hero>
+      <Services />
+    </>
+  );
 };
 
 export default Home;
